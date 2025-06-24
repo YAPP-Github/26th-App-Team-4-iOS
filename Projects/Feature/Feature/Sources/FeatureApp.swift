ee758ee64524f3df5dd9a570040cd279bf9d5765
JDeoks <duginee@naver.com>
2025-06-25 08:38:32 +0900

    [CHORE] Feature 모듈 추가

---
+++ Projects/Feature/Feature/Sources/FeatureApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct FeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Projects/Feature/Project.swift
@@ -0,0 +1,34 @@
+import ProjectDescription
+
+let project = Project(
+    name: "Feature",
+    targets: [
+        .target(
+            name: "Feature",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.Feature",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["Feature/Sources/**"],
+            resources: ["Feature/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "FeatureTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.FeatureTests",
+            infoPlist: .default,
+            sources: ["Feature/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "Feature")]
+        ),
+    ]
+)
