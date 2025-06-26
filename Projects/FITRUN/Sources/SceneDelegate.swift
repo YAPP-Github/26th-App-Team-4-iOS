3979940f9f249807bba1912efb5f1d52c49ddc0f
JDeoks <duginee@naver.com>
2025-06-26 11:36:04 +0900

    [CHORE] 소스폴더에 더미파일 추가

--- Projects/FITRUN/Sources/SceneDelegate.swift
+++ Projects/FITRUN/Sources/SceneDelegate.swift
@@ -12,13 +12,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
 
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    print("Hello SceneDelegate!")
-    guard let windowScene = (scene as? UIWindowScene) else { return }
-    window = UIWindow(windowScene: windowScene)
-    let viewController = ViewController()
-    window?.rootViewController = viewController
-    window?.makeKeyAndVisible()
-    guard let _ = (scene as? UIWindowScene) else { return }
+//    print("Hello SceneDelegate!")
+//    guard let windowScene = (scene as? UIWindowScene) else { return }
+//    window = UIWindow(windowScene: windowScene)
+//    let viewController = ViewController()
+//    window?.rootViewController = viewController
+//    window?.makeKeyAndVisible()
+//    guard let _ = (scene as? UIWindowScene) else { return }
+      let window = UIWindow(frame: UIScreen.main.bounds)
+      let rootVC = LoginCoordinator().start()
+      window.rootViewController = rootVC
+      window.makeKeyAndVisible()
+      self.window = window
   }
 
   func sceneDidDisconnect(_ scene: UIScene) {
