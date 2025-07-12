a0d1b0e238ddc6e61697d82ff47ae533b98e40b9
JDeoks <duginee@naver.com>
2025-07-13 00:11:49 +0900

    [FEAT] UITableView+ 추가

---
+++ Projects/Core/Core/Sources/Extension/UIKit/UITableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  UITableViewCell.swift
+//  Core
+//
+//  Created by JDeoks on 7/12/25.
+//
+
+import UIKit
+
+
+extension UITableViewCell {
+  public static var identifier: String {
+    return String(describing: self)
+  }
+}
