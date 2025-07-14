80e2f93d4c430125743705a0583bab0421b0654d
JDeoks <duginee@naver.com>
2025-07-14 20:50:34 +0900

    [FEAT] CommonNetworkHeaders에 토큰 추가

--- Projects/Data/Data/Sources/Network/Common/CommonNetworkHeaders.swift
+++ Projects/Data/Data/Sources/Network/Common/CommonNetworkHeaders.swift
@@ -5,11 +5,21 @@
 //  Created by dong eun shin on 7/8/25.
 //
 
+
 import Foundation
 
 public struct CommonNetworkHeaders {
-  public static let `default`: [String: String] = [
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  ]
+  /// 기본 헤더 + 동적으로 저장된 Access Token 을 붙입니다.
+  public static var `default`: [String: String] {
+    var headers: [String: String] = [
+      "Content-Type": "application/json"
+    ]
+
+    // AuthTokenStorageType 을 구현한 싱글톤 또는 DI 컨테이너에서 가져오세요.
+    if let token = AuthTokenStorage().getAccessToken() {
+      headers["Authorization"] = "Bearer \(token)"
+    }
+
+    return headers
+  }
 }
