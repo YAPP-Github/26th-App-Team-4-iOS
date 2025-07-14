06003f167aae964b14f7d38547c634470e27b993
JDeoks <duginee@naver.com>
2025-07-14 20:07:23 +0900

    [FEAT] NetworkProvider로깅 추가

--- Projects/Data/Data/Sources/Network/Base/NetworkProvider.swift
+++ Projects/Data/Data/Sources/Network/Base/NetworkProvider.swift
@@ -14,8 +14,13 @@ import Domain
 public final class NetworkProvider<Target: BaseAPI> {
   private let moyaProvider: MoyaProvider<Target>
 
-  public init(moyaProvider: MoyaProvider<Target>) {
-    self.moyaProvider = moyaProvider
+  public init(
+    plugins: [PluginType] = [
+      // 기본 헤더나 인증 인터셉터 플러그인이 있다면 여기에,
+      NetworkLoggerPlugin(configuration: .init(logOptions: .verbose))
+    ]
+  ) {
+    self.moyaProvider = MoyaProvider<Target>(plugins: plugins)
   }
 
   public func request(_ target: Target) -> Observable<Moya.Response> {
