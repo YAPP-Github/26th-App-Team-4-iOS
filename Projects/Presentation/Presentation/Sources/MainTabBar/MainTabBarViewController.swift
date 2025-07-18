541e0f625c88a4031692c5025e5b5e36236fa967
JDeoks <duginee@naver.com>
2025-07-18 20:28:21 +0900

    [FEAT] 메인 탭바에 홈 뷰컨 추가

--- Projects/Presentation/Presentation/Sources/MainTabBar/MainTabBarViewController.swift
+++ Projects/Presentation/Presentation/Sources/MainTabBar/MainTabBarViewController.swift
@@ -9,13 +9,43 @@ import UIKit
 import Core
 
 public final class MainTabBarController: UITabBarController {
+  
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+    setupTabs()
+  }
+  
+  private func setupTabs() {
+    let homeVC = HomeViewController()
+    let homeNav = UINavigationController(rootViewController: homeVC)
+    
+    homeNav.tabBarItem = UITabBarItem(
+      title: "홈",
+      image: UIImage(systemName: "house"),
+      selectedImage: UIImage(systemName: "house")
+    )
+    
+    viewControllers = [
+      homeNav
+    ]
+    
+    tabBar.tintColor = .black
+    tabBar.unselectedItemTintColor = .gray
+    tabBar.backgroundColor = .white//.mainBackground
+    tabBar.isTranslucent = false
 
-    public override func viewDidLoad() {
-        super.viewDidLoad()
-
+//    tabBar.layer.shadowColor = UIColor.black.cgColor
+//    tabBar.layer.shadowOffset = CGSize(width: 0, height: 2)
+//    tabBar.layer.shadowRadius = 3
+//    tabBar.layer.shadowOpacity = 0.2
+//    tabBar.layer.masksToBounds = false
+//    tabBar.shadowImage = UIImage()
+    
+    let tabBarAppearance = UITabBarAppearance()
+    tabBarAppearance.configureWithTransparentBackground()
+    tabBar.standardAppearance = tabBarAppearance
+    if #available(iOS 15.0, *) {
+      tabBar.scrollEdgeAppearance = tabBarAppearance
     }
-
-    private func setupTabs() {
-
-    }
+  }
 }
