a275c1a81dd2a54e017d18cfb50104fda14fd6b8
JDeoks <duginee@naver.com>
2025-07-18 20:29:24 +0900

    [CHORE] uiview 익스텐션 추가

---
+++ Projects/Core/Core/Sources/Extension/UIKit/UIView+.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+.swift
+//  Core
+//
+//  Created by JDeoks on 7/18/25.
+//
+
+
+import UIKit
+
+extension UIView {
+  public func addShadow(color: UIColor = .black, Opacity: Float = 0.10, Offset: CGSize = .init(width: 0, height: 1), radius: CGFloat = 4) {
+    self.layer.masksToBounds = false
+    self.layer.shadowColor = color.cgColor
+    self.layer.shadowOpacity = Opacity
+    self.layer.shadowOffset = Offset
+    self.layer.shadowRadius = radius
+  }
+}
