29d989b67c05b33c9528721523a4393c205becc8
JDeoks <duginee@naver.com>
2025-07-19 20:44:11 +0900

    [FEAT] DTO 추가

--- Projects/Data/Data/Sources/Network/API/GoalAPI.swift
+++ Projects/Data/Data/Sources/Network/API/GoalAPI.swift
@@ -9,17 +9,34 @@ import Foundation
 import Moya
 
 public enum GoalAPI: BaseAPI {
+  /// 러닝 동기 저장
   case savePurpose(runningPurpose: String)
+  /// 목표 조회
+  case goal
+  /// 페이스 목표 저장 (밀리초)
+  case savePace(paceGoalMs: Int)
+  /// 주간 러닝 횟수 저장
+  case saveRunningCount(weeklyRunningCount: Int)
 
   public var path: String {
     switch self {
-    case .savePurpose: return "/users/goals/purpose"
+    case .savePurpose: 
+      return "/users/goals/purpose"
+    case .goal:
+      return "/users/goals"
+    case .savePace:
+      return"/users/goals/pace"
+    case .saveRunningCount:
+      return "/users/goals/weekly-run-count"
     }
   }
 
   public var method: Moya.Method {
     switch self {
     case .savePurpose: return .post
+    case .goal: return .get
+    case .savePace: return .post
+    case .saveRunningCount: return .post
     }
   }
   
@@ -27,6 +44,15 @@ public enum GoalAPI: BaseAPI {
     switch self {
     case .savePurpose(let runningPurpose):
       return .requestParameters(parameters: ["runningPurpose": runningPurpose], encoding: JSONEncoding.default)
+      
+    case .goal:
+      return .requestParameters(parameters: [:], encoding: URLEncoding.default)
+      
+    case .savePace(paceGoalMs: let paceGoalMs):
+      return .requestParameters(parameters: ["pace": paceGoalMs], encoding: JSONEncoding.default)
+      
+    case .saveRunningCount(weeklyRunningCount: let weeklyRunningCount):
+      return .requestParameters(parameters: ["count": weeklyRunningCount], encoding: JSONEncoding.default)
     }
   }
 }
