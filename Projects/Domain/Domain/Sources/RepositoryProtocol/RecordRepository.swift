d99c4d519f79a65fecbe2021090177161490ce16
JDeoks <duginee@naver.com>
2025-07-31 09:14:59 +0900

    [FEAT] RecordRepository 추가

---
+++ Projects/Domain/Domain/Sources/RepositoryProtocol/RecordRepository.swift
@@ -0,0 +1,13 @@
+//
+//  RecordRepository.swift
+//  Domain
+//
+//  Created by JDeoks on 7/31/25.
+//
+
+import RxSwift
+
+
+public protocol RecordRepository {
+  func fetchRecordData(page: Int, size: Int) -> Single<RunningRecordList>
+}
