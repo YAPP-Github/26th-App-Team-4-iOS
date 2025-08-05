81376aef21b9e361dd0cce78561f38a9b8785aae
dongeunshin <7017410@naver.com>
2025-08-06 07:12:54 +0900

    [FEAT] AudioAPI 구현

---
+++ Projects/Data/Data/Sources/Network/API/AudioAPI.swift
@@ -0,0 +1,51 @@
+//
+//  AudioAPI.swift
+//  Data
+//
+//  Created by dong eun shin on 8/5/25.
+//
+
+import Foundation
+import Moya
+
+public enum AudioAPI: BaseAPI {
+  case coach
+  case runningInfo(paceMills: String)
+  case distance(type: String)
+  case paceFeedback(type: String)
+  case timeFeedback(type: String)
+
+  public var path: String {
+    switch self {
+    case .coach:
+      return "/audios/coach"
+    case .runningInfo:
+      return "/audios/running-info"
+    case .distance:
+      return "/audios/goals/distance"
+    case .paceFeedback:
+      return "/audios/goals/pace"
+    case .timeFeedback:
+      return "/audios/goals/time"
+    }
+  }
+  
+  public var method: Moya.Method {
+    return .get
+  }
+  
+  public var task: Task {
+    switch self {
+    case .coach:
+      return .requestPlain
+    case .runningInfo(let paceMills):
+      return .requestParameters(parameters: ["paceMills": paceMills], encoding: URLEncoding.default)
+    case .distance(let type):
+      return .requestParameters(parameters: ["type": type], encoding: URLEncoding.default)
+    case .paceFeedback(let type):
+      return .requestParameters(parameters: ["type": type], encoding: URLEncoding.default)
+    case .timeFeedback(let type):
+      return .requestParameters(parameters: ["type": type], encoding: URLEncoding.default)
+    }
+  }
+}
