c74f5e8294b02fe12d81d6a644f2fbf24a4632e0
dongeunshin <7017410@naver.com>
2025-08-13 10:54:10 +0900

    [FIX] 자동로그인 로직 오류 수정

--- Projects/Presentation/Presentation/Sources/Launch/LaunchViewController.swift
+++ Projects/Presentation/Presentation/Sources/Launch/LaunchViewController.swift
@@ -39,7 +39,7 @@ public final class LaunchViewController: BaseViewController, View {
       .compactMap { $0 }
       .subscribe(with: self) { object, status in
         switch status {
-          // TODO: - needsLogin시 로그인으로 보내야할지 논의 필요
+          // TODO: - needsWalkthrough는 실행 한번만 할 건지 논의 필요
         case .needsWalkthrough, .needsLogin:
           object.coordinator?.showWalkthrough()
         case .loggedIn:
