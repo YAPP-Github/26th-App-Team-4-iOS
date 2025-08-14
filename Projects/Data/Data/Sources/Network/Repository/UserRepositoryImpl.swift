4af515fb92cd54ab71c8d098ec6f96a35d7a58ea
JDeoks <duginee@naver.com>
2025-08-14 20:34:31 +0900

    [FEAT] add UserUseCase

---
+++ Projects/Data/Data/Sources/Network/Repository/UserRepositoryImpl.swift
@@ -0,0 +1,33 @@
+//
+//  UserRepositoryImpl.swift
+//  Data
+//
+//  Created by JDeoks on 8/13/25.
+//
+
+
+import Foundation
+import RxSwift
+import Domain
+
+public final class UserRepositoryImpl: UserRepository {
+  
+  private let provider: NetworkProvider<UserAPI>
+  
+  public init(provider: NetworkProvider<UserAPI> = .init()) {
+    self.provider = provider
+  }
+    
+  public func fetchUserInfo() -> RxSwift.Single<Domain.ProfileInfo> {
+    return provider.request(.fetchMyProfile)
+      .filterSuccessfulStatusCodes()
+      .map(APIResponse<ProfileInfoDTO>.self)
+      .compactMap { $0.result?.toDomain() }
+      .asSingle()
+      .catch { error in
+        // Handle the error appropriately, e.g., log it or convert it to a domain-specific error
+        print("Error fetching user info: \(error)")
+        return Single.error(error)
+      }
+  }
+}
