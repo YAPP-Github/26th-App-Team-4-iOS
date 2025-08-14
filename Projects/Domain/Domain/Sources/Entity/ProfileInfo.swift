a7fc44e541641f5858e527d0abab574ec1a9c362
JDeoks <duginee@naver.com>
2025-08-14 20:34:19 +0900

    [FEAT] add ProfileInfoDTO

---
+++ Projects/Domain/Domain/Sources/Entity/ProfileInfo.swift
@@ -0,0 +1,31 @@
+//
+//  ProfileInfo.swift
+//  Domain
+//
+//  Created by JDeoks on 8/14/25.
+//
+
+public struct ProfileInfo: Equatable {
+  public let userId: Int
+  public let nickname: String
+  public let email: String
+  public let provider: String
+  public let runnerType: RunnerType
+  public let goal: GoalInfo
+}
+
+public struct GoalInfo: Equatable {
+  public let goalId: Int
+  public let runningPurpose: RunningPurpose
+  public let weeklyRunningCount: Int
+  public let paceGoal: Int
+  public let distanceMeterGoal: Double
+  public let timeGoal: Int
+  public let runnerType: RunnerType
+}
+
+public enum RunnerType: String, Decodable {
+  case beginner = "BEGINNER"
+  case intermediate = "INTERMEDIATE"
+  case expert = "EXPERT"
+}
